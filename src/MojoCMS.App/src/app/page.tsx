c4c775b0da0a1b882acae8d701e6c9f76ed31bd0
joey fawad <mojofawad@gmail.com>
2024-06-10 20:24:07 -0400

    proof of concept next fetch data from dotnet api

--- src/MojoCMS.App/src/app/page.tsx
+++ src/MojoCMS.App/src/app/page.tsx
@@ -1,10 +1,40 @@
 import styles from './page.module.css'
 
-export default function Home() {
+async function getWeather() {
+    const cmsApi = process.env['services__api__http__0']
+    const res = await fetch(`${cmsApi}/weatherforecast`)
+    if (!res.ok) {
+        throw new Error('Failed to fetch weather data')
+    }
+    return res.json()
+}
+export default async function Home() {
+    const data = await getWeather();
     return (
+        
         <div className={styles.contentWrapper}>
             {/* TODO: Figure out component/page structure */}
             <p>Home</p>
+            <table className={styles.weatherTable}>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Temp C</th>
+                        <th>Temp F</th>
+                        <th>Summary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((data: { date: string, temperatureC: number, temperatureF: number, summary: string }) => (
+                        <tr key={data.date}>
+                            <td>{data.date}</td>
+                            <td>{data.temperatureC}</td>
+                            <td>{data.temperatureF}</td>
+                            <td>{data.summary}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
