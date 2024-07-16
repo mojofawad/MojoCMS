ab8349f9f61265fa8ce35c8ee54c5c4e0ed701d2
mojofawad <mojofawad@gmail.com>
2024-07-15 21:34:25 -0400

    add table using db data through the api

--- src/MojoCMS.App/src/app/page.tsx
+++ src/MojoCMS.App/src/app/page.tsx
@@ -8,8 +8,18 @@ async function getWeather() {
     }
     return res.json()
 }
+
+async function getBlogs() {
+    const cmsApi = process.env['services__api__http__0']
+    const res = await fetch(`${cmsApi}/blogs`)
+    if (!res.ok) {
+        throw new Error('Failed to fetch blog data')
+    }
+    return res.json()
+}
 export default async function Home() {
     const data = await getWeather();
+    const blogData = await getBlogs();
     return (
         
         <div className={styles.contentWrapper}>
@@ -33,6 +43,26 @@ export default async function Home() {
                             <td>{data.summary}</td>
                         </tr>
                     ))}
+                </tbody>
+            </table>
+            <table className={styles.blogTable}>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Title</th>
+                        <th>CreatedDate</th>
+                        <th>UpdatedDate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {blogData.map((blogData: { id: string, name: string, createdAt: string, updatedAt: string}) => (
+                    <tr>
+                        <td>{blogData.id}</td>
+                        <td>{blogData.name}</td>
+                        <td>{blogData.createdAt}</td>
+                        <td>{blogData.updatedAt}</td>
+                    </tr>
+                ))}
                 </tbody>
             </table>
         </div>
